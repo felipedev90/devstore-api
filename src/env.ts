774f617b765ff933ee65b9e6de9f2615006aaffd
Felipe Augusto <augusto.felipedev@gmail.com>
2026-04-10 18:50:43 -0300

    feat: add enviroment variables validation with zod and dotenv

---
+++ src/env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+import "dotenv/config";
+
+// Define o schema para as variáveis de ambiente usando zod
+const schema = z.object({
+  PORT: z.string(), // "PORT" é uma variável de ambiente obrigatória do tipo string
+});
+
+// Faz o parse das variáveis de ambiente usando o schema definido
+// Se as variáveis de ambiente não estiverem de acordo com o schema, um erro será lançado
+export const env = schema.parse(process.env);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from "./app";
+import { env } from "./env";
 
 async function start() {
   try {
     // await app.listen({ port: 3333, host: '0.0.0.0' }); => para aceitar conexões de fora do localhost
-    await app.listen({ port: 3333 });
-    console.log("Server is running on port 3333");
+    await app.listen({ port: Number(env.PORT) });
+    console.log(`Server is running on port ${env.PORT}`);
   } catch (err) {
     console.error("Error starting server:", err);
     // encerra o processo com código de erro. O 1 é convenção universal pra "terminou com falha". 0 seria "terminou com sucesso"

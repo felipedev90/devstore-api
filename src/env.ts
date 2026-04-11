4319445c7d91fe8dc1a20a8b96a00d7aef70c716
Felipe Augusto <augusto.felipedev@gmail.com>
2026-04-11 17:13:02 -0300

    feat: add prisma with product model and database conecction

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 // Define o schema para as variáveis de ambiente usando zod
 const schema = z.object({
   PORT: z.string(), // "PORT" é uma variável de ambiente obrigatória do tipo string
+  DATABASE_URL: z.string(),
 });
 
 // Faz o parse das variáveis de ambiente usando o schema definido

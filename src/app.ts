7af2fefd6f23e2f121978cf4bb80be415109a655
Felipe Augusto <augusto.felipedev@gmail.com>
2026-04-11 18:16:06 -0300

    feat: add GET and POST routes for products

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import fastify from "fastify";
+import { productRoutes } from "./routes/products";
 
 export const app = fastify();
 
 app.get("/health", async (request, reply) => {
   return { status: "ok" };
 });
+
+app.register(productRoutes);
---
+++ src/routes/products.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import z from "zod";
+
+const createProductSchema = z.object({
+  name: z.string(),
+  price: z.number(),
+  description: z.string(),
+  stock: z.number().int(),
+});
+
+export async function productRoutes(app: FastifyInstance) {
+  app.get("/products", async (request, reply) => {
+    const products = await prisma.product.findMany();
+    return reply.send(products);
+  });
+
+  app.post("/products", async (request, reply) => {
+    const result = createProductSchema.safeParse(request.body);
+
+    if (!result.success) {
+      return reply.status(400).send({ error: result.error.format() });
+    }
+
+    const product = await prisma.product.create({ data: result.data });
+    return reply.status(201).send(product);
+  });
+}
